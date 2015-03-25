e101faffb4681c8ca71bb3e431c1b87e52e15d39
Tomasz Milczarek <tomasz.milczarek@gmail.com>
2015-03-25 17:55:53 +0100

    add model class Judgement

---
+++ app/src/main/java/com/plumya/jurisprudenceon/model/Judgement.java
@@ -0,0 +1,21 @@
+package com.plumya.jurisprudenceon.model;
+
+import java.util.Date;
+
+/**
+ * Created by toml on 25.03.15.
+ */
+public class Judgement {
+
+    public int judgementId;
+
+    public String courtRoom;
+
+    public String signature;
+
+    public Date judgementDate;
+
+    public String issue;
+
+    public String decision;
+}
