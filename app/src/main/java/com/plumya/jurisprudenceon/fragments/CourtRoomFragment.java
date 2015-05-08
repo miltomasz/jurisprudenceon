d859b2b1ff927be820b8756209b96da840c791d3
Tomasz Milczarek <tomasz.milczarek@gmail.com>
2015-05-08 02:26:31 +0200

    Add swipe to refresh pattern

--- app/src/main/java/com/plumya/jurisprudenceon/fragments/CourtRoomFragment.java
+++ app/src/main/java/com/plumya/jurisprudenceon/fragments/CourtRoomFragment.java
@@ -2,23 +2,30 @@ package com.plumya.jurisprudenceon.fragments;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.parse.ParseObject;
 import com.parse.ParseQueryAdapter;
+import com.plumya.jurisprudenceon.MainActivity;
 import com.plumya.jurisprudenceon.R;
 import com.plumya.jurisprudenceon.app.CourtRoomAdapter;
 
 import java.util.Locale;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by toml on 20.03.15.
@@ -28,6 +35,9 @@ public class CourtRoomFragment extends Fragment {
     private String courtRoom;
     private ParseQueryAdapter<ParseObject> mainAdapter;
     private ListView listView;
+
+    @InjectView(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout swipeRefreshLayout;
 
     public static CourtRoomFragment newInstance(int position) {
         CourtRoomFragment fragment = new CourtRoomFragment();
@@ -44,12 +54,16 @@ public class CourtRoomFragment extends Fragment {
         courtRoom = getResources().getStringArray(R.array.court_rooms_array)[courtRoomNumber];
         int imageId = getResources().getIdentifier(courtRoom.toLowerCase(Locale.getDefault()),
                 "drawable", getActivity().getPackageName());
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.court_room_fragment, container, false);
+        ButterKnife.inject(this, rootView);
+
         listView = (ListView) rootView.findViewById(R.id.judgement_list);
         // Initialize main ParseQueryAdapter
         mainAdapter = new CourtRoomAdapter(getActivity());
@@ -57,6 +71,22 @@ public class CourtRoomFragment extends Fragment {
 
         listView.setAdapter(mainAdapter);
         mainAdapter.loadObjects();
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshContent();
+            }
+            private void refreshContent() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mainAdapter.loadObjects();
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 1000);
+            }
+        });
 
         return rootView;
     }
